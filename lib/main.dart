c8c38673628d3538d5598646631c0a1839cd3302
Lukas <lukas@becker-development.de>
2020-11-14 15:19:35 +0100

    Added Pantry and search

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,170 @@
-import 'dart:ffi';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(APITest());
+  runApp(TabNavigation());
 }
 
-class APITest extends StatelessWidget {
+//Tab Navigation
+class TabNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple API demo',
       theme: ThemeData(
         primarySwatch: Colors.lime,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FirstRecipes(title: 'Get your first recipe'),
+      home: DefaultTabController(
+        length: 3,
+        initialIndex: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.kitchen)),
+                Tab(icon: Icon(Icons.fastfood_outlined)),
+                Tab(icon: Icon(Icons.food_bank_outlined)),
+              ],
+            ),
+            title: Text('Food App '),
+          ),
+          body: TabBarView(
+            children: [
+              PantryWidget(),
+              RecipeListWidget(),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class FirstRecipes extends StatefulWidget {
-  FirstRecipes({Key key, this.title}) : super(key: key);
+//First Tab - Pantry
+class PantryWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Pantry(),
+    );
+  }
+}
+
+class Pantry extends StatefulWidget{
+
+  @override
+  _PantryState createState() => _PantryState();
+}
+
+class _PantryState extends State<Pantry> {
+  final List<String> entries = <String>['egg', 'pork', 'cheese'];
+
+  var tController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        body: Center(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[100],
+                child: Center(child: Text(entries[index])),
+              );
+            },
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            String newItem;
+
+            showDialog(
+                context: context,
+                builder: (BuildContext context){
+                  return AlertDialog(
+                    title: Text("New List Item"),
+                    content: Row(
+                      children: [
+                        Expanded(child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: tController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'New Ingredient',
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
+                    actions: [
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Close")
+                      ),
+                      FlatButton(
+                      child: Text("Save"),
+                      onPressed: (){
+                        newItem = tController.text;
+                        Navigator.of(context).pop();
+                      },
+                      ),
+
+                    ],
+                  );
+                }
+            ).then((value) => setState(() {
+              if(newItem != null)
+                entries.insert(entries.length,newItem);
+            }));
+
+
+
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.lime,
+        ),
+    );
+  }
+}
+//End of Pantry
+
+//Center Tab Recipe List
+class RecipeListWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Recipes(title: 'Get your first recipe'),
+    );
+  }
+}
+
+class Recipes extends StatefulWidget {
+  Recipes({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -39,16 +178,21 @@ class FirstRecipes extends StatefulWidget {
   final String title;
 
   @override
-  _FirstRecipesState createState() => _FirstRecipesState();
+  _RecipesState createState() => _RecipesState();
 }
 
-class _FirstRecipesState extends State<FirstRecipes> {
+class _RecipesState extends State<Recipes> {
   var futureRecipes = [];
   Future<dynamic> futureJson;
   int count = 0;
 
+  String ingredients = 'pork';
+
+  final tController = new TextEditingController();
+
   Future<dynamic> fetchJson() async {
-    final response = await http.get('http://www.recipepuppy.com/api/?i=pork');
+    print('http://www.recipepuppy.com/api/?i=' + ingredients);
+    final response = await http.get('http://www.recipepuppy.com/api/?i=' + ingredients);
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
@@ -63,7 +207,7 @@ class _FirstRecipesState extends State<FirstRecipes> {
   }
 
   Future<Recipe> fetchRecipe(int resNumber) async {
-    final response = await http.get('http://www.recipepuppy.com/api/?i=pork');
+    final response = await http.get('http://www.recipepuppy.com/api/?i=' + ingredients);
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
@@ -105,18 +249,19 @@ class _FirstRecipesState extends State<FirstRecipes> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
+    return Scaffold(/*
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
+      ),*/
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
 
         child: SingleChildScrollView(
-          child: Column(children: _printRecipes()),
+          child: Column(
+            children: _printRecipes()),
         )
       )
     );
@@ -124,10 +269,40 @@ class _FirstRecipesState extends State<FirstRecipes> {
 
   List<Widget> _printRecipes(){
     List<Widget> children = new List();
-    children.add(Text(
-      'Fresh Recipes:',
-      style: Theme.of(context).textTheme.headline2,
-    ));
+    children.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+          children: [
+            Expanded(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: tController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Ingredients',
+                ),
+              ),
+            )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                color: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    this.count = 0;
+                    this.futureRecipes.clear();
+                    this.futureJson = null;
+                    this.ingredients = tController.text;
+                    this.futureJson = fetchJson().then((value) => this.complete(value));
+                  });
+
+                },
+                child: Text("Search"),),
+            )
+          ],
+        )
+    );
     int i;
     for(i = 0; i < count; i++){
 
@@ -196,23 +371,26 @@ class _FirstRecipesState extends State<FirstRecipes> {
   }
 }
 
-  class Recipe {
-    final String title;
-    final String href;
-    final String ingredients;
-    final String thumbnail;
+class Recipe {
+  final String title;
+  final String href;
+  final String ingredients;
+  final String thumbnail;
 
-    Recipe({this.title, this.href, this.ingredients, this.thumbnail});
+  Recipe({this.title, this.href, this.ingredients, this.thumbnail});
 
-    factory Recipe.fromJson(Map<String, dynamic> json) {
-      return Recipe(
-        title: json['title'],
-        href: json['href'],
-        ingredients: json['ingredients'],
-        thumbnail: json['thumbnail'],
-      );
-    }
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      title: json['title'],
+      href: json['href'],
+      ingredients: json['ingredients'],
+      thumbnail: json['thumbnail'],
+    );
   }
+}
+//End of Recipe list
+
+
 
 
 
